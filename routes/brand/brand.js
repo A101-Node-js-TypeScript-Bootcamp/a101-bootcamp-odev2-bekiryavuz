6255a20f4e9ac591659889b95e7b6a64f547ceda
Bekir YAVUZ <byculs@gmail.com>
2022-01-17 21:45:01 +0300

    trendyol get done
    added brands road
    added categories
    added singleCategory

--- routes/brand/brand.js
+++ routes/brand/brand.js
@@ -4,10 +4,9 @@ let router = express.Router();
 let brandController = require('../../controllers/brand')
 
 
-router.get('/:id', brandController.fetchIdBrands)
-router.get('/:name', brandController.fetchNameBrands)
+router.get('/brands', brandController.fetchBrands)
 router.get('/categories',brandController.fetchCategories)
-router.get('/category',brandController.fetchCategory)
+router.get('/category/:id',brandController.fetchSingleCategory)
 
 
 
--- services/brand.js
+++ services/brand.js
@@ -1,18 +1,46 @@
 const axios = require ('axios')
 
-exports.fetchIdBrands = async () =>{
-    const response = await axios.get('https://api.trendyol.com/sapigw/brands');
-    return response.data;
+
+
+exports.fetchBrands = async (req,res) =>{
+    try{
+        const response = await axios.get('https://api.trendyol.com/sapigw/brands');
+        return {
+            status : true,
+            data : response.data
+        }
+    }catch{
+        return{
+            status : false,
+            message : 'böyle bir data mevcut değil'
+        }
+    }   
 }
-exports.fetchNameBrands = async (brandName) =>{
-    const response = await axios.get('https://api.trendyol.com/sapigw/brands/by-name?name={brandName}');
-    return response.data;
-}/*
-exports.fetch = async () =>{
-    const response = await axios.get(`https://api.trendyol.com/sapigw/product-categories/${}/attributes`);
-    return response.data;
+exports.categories = async (req,res) =>{
+    try{
+        const response = await axios.get('https://api.trendyol.com/sapigw/product-categories');
+        return {
+            status : true,
+            data : response.data
+        }
+    }catch{
+        return{
+            status : false,
+            message : 'böyle bir data mevcut değil'
+        }
+    }  
 }
-exports.fetch = async () =>{
-    const response = await axios.get(`https://api.trendyol.com/sapigw/product-categories/${}/attributes`);
-    return response.data;
-}*/
+exports.singleCategory = async (id) =>{
+    try{
+        const response = await axios.get(`https://api.trendyol.com/sapigw/product-categories/${id}/attributes`);
+        return {
+            status : true,
+            data : response.data
+        }
+    }catch{
+        return{
+            status : false,
+            message : 'böyle bir data mevcut değil'
+        }
+    }   
+}
